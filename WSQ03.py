0b8b0b7da38f5eab118172a8c19416e5e2b52f20
OscarEduardo <Oscar_789456123@hotmail.com>
2015-03-04 18:15:38 -0600

    Create WSQ03.py

---
+++ WSQ03.py
@@ -0,0 +1,16 @@
+# WSQ
+#Oscar Eduardo sanchez
+print("Fun with numbers")
+print("The program works with two numbers and do diferents calculates")
+x=int(input("Give me a number: "))
+y=int(input("Give me another number: "))
+sum= x+y
+res= x-y
+mult= x*y
+div= x//y
+Divr=x%y
+print("The sum of the numbers is:", sum)
+print("The difference of the numbers is:", res)
+print("The product of the numbers is:", mult)
+print("The division of the numbers is:", div)
+print("The remainder of the numbers is:",Divr)
